11b629bdb029ee9519067984975fb0ea26053a3e
Ace Baugh <ace.d.baugh@gmail.com>
2025-07-17 21:53:35 -0400

    Adding CBA to Git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <div className="test">
+          <div className="drawing">
+            <img src="/src/assets/images/house-draw-trans-pillow.png" alt="drawing" />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
